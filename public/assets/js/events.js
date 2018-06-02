f450608f0c50de99c55e45e74a960a7988440d93
ShemManyu <smanyu48@gmail.com>
2018-06-02 22:48:04 +0300

    finished getResults logic

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -1,3 +1,4 @@
+candidates = 2;
 function httpGetAsync(theUrl, callback)
 {
     var xmlHttp = new XMLHttpRequest();
@@ -10,7 +11,47 @@ function httpGetAsync(theUrl, callback)
 }
 function getResults(){
     console.log("Got votes");
-    httpGetAsync("http://localhost:8080/count/2", function(data) {
-        console.log(data); 
-    });
+    //Check if list already exists and remove it if it does
+    //to update it with new values
+    exists = !!document.getElementById("candidatesList");
+    if (exists){
+        let element = document.getElementById("candidatesList");
+        element.parentNode.removeChild(element);
+    }
+
+    //Create list
+    var ul = document.createElement('ul');
+    ul.setAttribute('id','candidatesList');
+    document.getElementById('resultslist').appendChild(ul);
+
+    //Add candidate
+    function renderCandidateList(candidate, count) {
+        let li = document.createElement('li');
+        li.setAttribute('class','candidate');
+        
+        var candidateCount;
+        var t;
+        if (candidate == 1){
+            candidateCount = "John Doe has " + count + " votes";
+        } else if (candidate == 2){
+            candidateCount = "Mary Sue has " + count + " votes";
+        } else{
+            //do something
+        }
+        ul.appendChild(li);
+        
+        t = document.createTextNode(candidateCount);
+        
+        li.innerHTML=li.innerHTML + candidateCount;
+    }
+
+
+    //fetch results
+    for (let index = 1; index <= candidates; index++) {
+        httpGetAsync("http://localhost:8080/count/" + index, function(data) {
+            renderCandidateList(index, data);
+            console.log("Candidate " + index + " got " + data + " votes"); 
+        });
+    }
 }
+
